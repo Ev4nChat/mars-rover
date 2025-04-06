2b02c5dce441c4d51619f7df085d519a971a17fd
Evangelos Chatzistefanou <ev4nchat@gmail.com>
2025-04-06 10:44:43 +0300

    Add tests for plateau and rover

---
+++ tests/PlateauTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Tests;
+
+use App\Plateau;
+use PHPUnit\Framework\TestCase;
+
+Class PlateauTest extends TestCase
+{
+    public function testPlateau(): void
+    {
+        $plateau = new Plateau(5, 5);
+
+        $this->assertTrue($plateau->isWithinLimits(0, 0));
+        $this->assertTrue($plateau->isWithinLimits(0, 5));
+        $this->assertTrue($plateau->isWithinLimits(5, 0));
+        $this->assertTrue($plateau->isWithinLimits(5, 5));
+        $this->assertTrue($plateau->isWithinLimits(1, 1));
+        $this->assertFalse($plateau->isWithinLimits(1, 6));
+    }
+}
---
+++ tests/RoverTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Tests;
+
+use App\Plateau;
+use App\Rover;
+use PHPUnit\Framework\TestCase;
+
+class RoverTest extends TestCase
+{
+    public function testTurnLeft(): void
+    {
+        $plateau = new Plateau(5, 5);
+        $rover = new Rover(1, 1, 'N', $plateau);
+        $result = $rover->readAndProcessInstructions('L');
+
+        $this->assertEquals('1 1 W', $result);
+    }
+
+    public function testTurnRight(): void
+    {
+        $plateau = new Plateau(5, 5);
+        $rover = new Rover(1, 1, 'N', $plateau);
+        $result = $rover->readAndProcessInstructions('R');
+
+        $this->assertEquals('1 1 E', $result);
+    }
+
+    public function testMoveNorth(): void
+    {
+        $plateau = new Plateau(5, 5);
+        $rover = new Rover(1, 1, 'N', $plateau);
+        $result = $rover->readAndProcessInstructions('M');
+
+        $this->assertEquals('1 2 N', $result);
+
+    }
+
+    public function testMoveEast(): void
+    {
+        $plateau = new Plateau(5, 5);
+        $rover = new Rover(1, 1, 'E', $plateau);
+        $result = $rover->readAndProcessInstructions('M');
+
+        $this->assertEquals('2 1 E', $result);
+    }
+
+    public function testMoveSouth(): void
+    {
+        $plateau = new Plateau(5, 5);
+        $rover = new Rover(1, 1, 'S', $plateau);
+        $result = $rover->readAndProcessInstructions('M');
+
+        $this->assertEquals('1 0 S', $result);
+    }
+
+    public function testMoveWest(): void
+    {
+        $plateau = new Plateau(5, 5);
+        $rover = new Rover(1, 1, 'W', $plateau);
+        $result = $rover->readAndProcessInstructions('M');
+
+        $this->assertEquals('0 1 W', $result);
+    }
+
+    public function testReadAndProcessInstructions(): void
+    {
+        $plateau = new Plateau(5, 5);
+        $rover = new Rover(1, 1, 'N', $plateau);
+        $result = $rover->readAndProcessInstructions('LMRMMM');
+
+        $this->assertEquals('0 4 N', $result);
+    }
+
+    public function testMoveOutsideLimits(): void
+    {
+        $plateau = new Plateau(5, 5);
+        $rover = new Rover(0, 1, 'W', $plateau);
+        $result = $rover->readAndProcessInstructions('M');
+
+        $this->assertEquals('0 1 W', $result);
+    }
+
+}
